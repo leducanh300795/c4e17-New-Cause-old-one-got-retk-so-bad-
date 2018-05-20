cd271ad8507170e4d45acdc76fddced27a1c1962
leducanh300795 <leducanh300795@gmail.com>
2018-05-20 16:23:28 +0700

    intial commit

---
+++ Webmodule/Session3/testdes.py
@@ -0,0 +1,20 @@
+from random import randint,choice
+def measurements():
+    mesurements = [randint(85,95), randint(55,65), randint(85,95)]
+
+    return (mesurements)
+mesure = measurements()
+print(mesure)
+
+def description():
+    characters = ['dễ dãi', 'vui vẻ', 'hòa đồng', 'yêu đời',
+                  'khéo léo', 'hài hước', 'quái thai', 'điên dại',
+                  'tình cảm', 'sida', 'hâm', 'cẩn thận',
+                  'nổi loạn', 'cầu toàn', 'bốc đồng']
+    description = []
+    for i in range(4):
+        character = choice(characters)
+        description.append(character)
+    return (description)
+des = description()
+print(*des, sep=', ')
